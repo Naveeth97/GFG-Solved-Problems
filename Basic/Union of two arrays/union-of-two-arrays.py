22b8a5765e687e14f05661a69a24bc3d901621bc
Naveeth Khan J <111632946+Naveeth97@users.noreply.github.com>
2023-08-12 19:42:56 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Union of two arrays/union-of-two-arrays.py
@@ -0,0 +1,31 @@
+#User function Template for python3
+
+class Solution:    
+    #Function to return the count of number of elements in union of two arrays.
+    def doUnion(self,a,n,b,m):
+        #code here
+        s1 = set()
+        for i in range(n):
+            s1.add(a[i])
+        for j in range(m):
+            s1.add(b[j])
+        return len(s1)
+            
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(t):
+        
+        n,m=[int(x) for x in input().strip().split()]
+        
+        a=[int(x) for x in input().strip().split()]
+        b=[int(x) for x in input().strip().split()]
+        ob=Solution()
+        
+        print(ob.doUnion(a,n,b,m))
+# } Driver Code Ends
