33d68bc7ae08e62911f84f09fe96e7dfdd87670d
Naveeth Khan J <111632946+Naveeth97@users.noreply.github.com>
2023-08-25 21:43:49 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Square root of a number/square-root-of-a-number.py
@@ -0,0 +1,33 @@
+#User function Template for python3
+
+import math
+#Complete this function
+class Solution:
+    def floorSqrt(self, x): 
+    #Your code here
+        y = math.sqrt(x)
+        return math.floor(y)
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+
+def main():
+        T=int(input())
+        while(T>0):
+            
+            x=int(input())
+            
+            print(Solution().floorSqrt(x))
+            
+            T-=1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
