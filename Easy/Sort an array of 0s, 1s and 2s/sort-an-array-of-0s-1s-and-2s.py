09efb45365a5784efeba11e235872e1438443ea8
Naveeth Khan J <111632946+Naveeth97@users.noreply.github.com>
2023-09-10 22:40:28 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Sort an array of 0s, 1s and 2s/sort-an-array-of-0s-1s-and-2s.py
@@ -0,0 +1,42 @@
+#User function Template for python3
+
+class Solution:
+    def sort012(self,arr,n):
+        # code here
+        #Dutch National Flag Algorithm
+        low,mid,high = 0,0,len(arr) - 1
+        while(mid<=high):
+            if(arr[mid] == 0):
+                flag = arr[low]
+                arr[low] = arr[mid]
+                arr[mid] = flag
+                low += 1
+                mid += 1
+            elif(arr[mid] == 1):
+                mid += 1
+            else:
+                flag = arr[high]
+                arr[high] = arr[mid]
+                arr[mid] = flag
+                high -= 1
+        return arr
+                
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t=int(input())
+    for _ in range(t):
+        n=int(input())
+        arr=[int(x) for x in input().strip().split()]
+        ob=Solution()
+        ob.sort012(arr,n)
+        for i in arr:
+            print(i, end=' ')
+        print()
+
+# } Driver Code Ends
