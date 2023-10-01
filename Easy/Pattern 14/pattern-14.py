fa5a3816e058f2e74663dac50d20e3c13b7a37d5
Naveeth Khan J <111632946+Naveeth97@users.noreply.github.com>
2023-10-01 17:45:32 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Pattern 14/pattern-14.py
@@ -0,0 +1,23 @@
+#User function Template for python3
+
+class Solution:
+    def printTriangle(self, N):
+        # Code here
+        for i in range(N):
+            
+            for j in range(65,65+i+1):
+                print(chr(j),end='')
+            print()
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input().strip())
+        ob = Solution()
+        ob.printTriangle(N)
+# } Driver Code Ends
