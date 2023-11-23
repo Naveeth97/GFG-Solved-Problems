a9e1362f08e85fb7443c2760a3aba279d87aa346
Naveeth Khan J <111632946+Naveeth97@users.noreply.github.com>
2023-11-23 21:09:22 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Find Missing And Repeating/find-missing-and-repeating.py
@@ -0,0 +1,48 @@
+#User function Template for python3
+
+class Solution:
+    #def missing(self,arr,n,lis):
+        
+        
+    def findTwoElement( self,arr, n): 
+        
+        sn = (n*(n+1))//2
+        s2n = (n*(n+1)*(2*n+1))//6
+        s1,s2 = 0, 0
+        repeating = -1
+        missing = -1
+        
+        for i in range(n):
+            s1 += arr[i]
+            s2 += (arr[i] * arr[i])
+            
+        val1 = sn - s1
+        val2 = s2n - s2
+        
+        val2 = val2//val1
+        
+        repeating = (val1 + val2)//2
+        missing = repeating - val1
+        
+        return [missing,repeating]
+        
+        
+        
+        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+
+    tc=int(input())
+    while tc > 0:
+        n=int(input())
+        arr=list(map(int, input().strip().split()))
+        ob = Solution()
+        ans=ob.findTwoElement(arr, n)
+        print(str(ans[0])+" "+str(ans[1]))
+        tc=tc-1
+# } Driver Code Ends
