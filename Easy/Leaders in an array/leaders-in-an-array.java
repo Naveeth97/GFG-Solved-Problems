288401d66d4fedcd23f315290926c99f84e07f93
Naveeth Khan J <111632946+Naveeth97@users.noreply.github.com>
2024-05-13 22:22:07 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Leaders in an array/leaders-in-an-array.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Array {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		PrintWriter out = new PrintWriter(System.out);
+		while(t-->0){
+		    
+		    //input size of array
+		    int n = Integer.parseInt(br.readLine().trim());
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    
+		    //inserting elements in the array
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    
+		    StringBuffer str = new StringBuffer();
+		    ArrayList<Integer> res = new ArrayList<Integer>();
+		  
+		    //calling leaders() function
+		    res = obj.leaders(arr, n);
+		    
+		    //appending result to a String
+		    for(int i=0; i<res.size(); i++){
+		        str.append(res.get(i)+" ");
+		    }
+		    
+		    //printing the String
+		    out.println(str);
+		}
+		out.flush();
+	}
+}
+
+// } Driver Code Ends
+
+
+class Solution{
+    //Function to find the leaders in the array.
+    static ArrayList<Integer> leaders(int arr[], int n){
+        // Your code here
+        ArrayList<Integer> arr1 = new ArrayList<Integer>();
+        arr1.add(arr[n - 1]);
+        if(n == 1){
+            return arr1;
+        }
+        int check = arr[n - 1];
+        for(int i = n - 2;i>=0;i--){
+            if(arr[i] >= check){
+                arr1.add(0, arr[i]);
+                check = arr[i];
+            }
+        }
+        
+        return arr1;
+    }
+}
