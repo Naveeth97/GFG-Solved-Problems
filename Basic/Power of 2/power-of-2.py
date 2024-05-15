77e4bcc7a50c9194ef2a885130f7556c2dbc9f0d
Naveeth Khan J <111632946+Naveeth97@users.noreply.github.com>
2024-05-15 23:04:16 +0530

    Updated Solution - GfG to GitHub

--- Basic/Power of 2/power-of-2.py
+++ Basic/Power of 2/power-of-2.py
@@ -3,16 +3,16 @@
 class Solution:
     ##Complete this function
     # Function to check if given number n is a power of two.
-    def isPowerofTwo(self,n):
+    def isPowerofTwo(self,n : int ) -> bool:
         ##Your code here
-        if(n==0):
+        
+        if(n == 0):
             return False
-        while(n!=1):
-            if(n%2 != 0):
-                return False
-            n//=2
-        return True
+        
+        if(n & (n - 1) == 0):
+            return True
             
+        return False
 
 
 #{ 
