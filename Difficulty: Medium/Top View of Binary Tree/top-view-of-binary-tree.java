4453639011452f4d29f4bf0ea8faba098a9beaff
Naveeth Khan J <111632946+Naveeth97@users.noreply.github.com>
2024-08-11 14:17:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.java
@@ -0,0 +1,179 @@
+//{ Driver Code Starts
+// Initial Template for JAVA
+
+import java.io.*;
+import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+/*
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+*/
+class Pair {
+    
+    Node root;
+    int data;
+    
+    Pair(Node _root, int _data) {
+        
+        root = _root;
+        data = _data;
+    }
+}
+
+class Solution {
+    // Function to return a list of nodes visible from the top view
+    // from left to right in Binary Tree.
+    static ArrayList<Integer> topView(Node root) {
+        
+        // add your code
+        
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        if (root == null) return result;
+        
+        Queue<Pair> queue = new LinkedList<>();
+        
+        queue.add(new Pair(root, 0));
+        
+        Map<Integer, Integer> topView = new TreeMap<>();
+        
+        while (!queue.isEmpty()) {
+            
+            Pair pair = queue.poll();
+            int order = pair.data;
+            Node temp = pair.root;
+            
+            if (temp.left != null) {
+                queue.add(new Pair(temp.left, order - 1));
+            }
+            
+            if (temp.right != null) {
+                queue.add(new Pair(temp.right, order + 1));
+            }
+            
+            if (topView.get(order) == null) topView.put(order, temp.data);
+        }
+        
+        for (Map.Entry<Integer, Integer> entry : topView.entrySet()) {
+            result.add(entry.getValue());
+        }
+        
+        return result;
+        
+    }
+}
+
+//{ Driver Code Starts.
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+public class Tree {
+
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    static void printInorder(Node root) {
+        if (root == null) return;
+
+        printInorder(root.left);
+        System.out.print(root.data + " ");
+
+        printInorder(root.right);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+
+            Solution ob = new Solution();
+
+            ArrayList<Integer> vec = ob.topView(root);
+            for (int x : vec) System.out.print(x + " ");
+            System.out.println();
+
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
