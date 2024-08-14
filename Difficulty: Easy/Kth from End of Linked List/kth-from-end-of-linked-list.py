8efa5a16760aa4f073b47e7bc9e0194348581741
Naveeth Khan J <111632946+Naveeth97@users.noreply.github.com>
2024-08-14 22:35:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Kth from End of Linked List/kth-from-end-of-linked-list.py
@@ -0,0 +1,87 @@
+#User function Template for python3
+'''
+	A linked list node has the following structure
+	{
+		# Node Class
+		class Node:
+		    def __init__(self, data):   # data -> value stored in node
+		        self.data = data
+		        self.next = None
+	}
+'''
+#Function to find the data of kth node from the end of a linked list
+class Solution:
+    def getKthFromLast(self, head, k):
+        #code here
+        
+        temp = head
+        k -= 1
+        
+        
+        while k >=1:
+            
+            temp = temp.next
+            
+            if temp == None:
+                return -1
+            k -= 1
+                
+                
+        while temp.next != None:
+            
+            temp = temp.next
+            head = head.next
+            
+        return head.data
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+# Define the Node class for the linked list
+class Node:
+
+    def __init__(self, x):
+        self.data = x
+        self.next = None
+
+
+# Function to print the linked list
+def printList(node):
+    while node:
+        print(node.data, end=" ")
+        node = node.next
+    print()
+
+
+#Position this line where user code will be pasted.
+
+# Main function
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.read
+    data = input().splitlines()
+
+    t = int(data[0].strip())
+    idx = 1
+
+    while t > 0:
+        arr = list(map(int, data[idx].strip().split()))
+        x = int(data[idx + 1].strip())
+        idx += 2
+
+        head = None
+        if arr:
+            head = Node(arr[0])
+            tail = head
+            for num in arr[1:]:
+                tail.next = Node(num)
+                tail = tail.next
+
+        ob = Solution()
+        print(ob.getKthFromLast(head, x))
+        t -= 1
+
+# } Driver Code Ends
