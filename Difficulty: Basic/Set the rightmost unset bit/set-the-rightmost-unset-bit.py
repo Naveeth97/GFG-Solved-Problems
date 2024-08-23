8fc13b8a09ad981873ff5b1c5fd8249445a80057
Naveeth Khan J <111632946+Naveeth97@users.noreply.github.com>
2024-08-23 11:01:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Set the rightmost unset bit/set-the-rightmost-unset-bit.py
@@ -0,0 +1,22 @@
+#User function Template for python3
+class Solution:
+	def setBit(self, n):
+		# code here
+		
+		return n | (n + 1)
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    T = int(input())
+    for i in range(T):
+        N = int(input())
+
+        ob = Solution()
+        ans = ob.setBit(N)
+        print(ans)
+
+# } Driver Code Ends
