3b4531866dfcc1e715b412b3538d4cc1a6259e71
Naveeth Khan J <111632946+Naveeth97@users.noreply.github.com>
2024-09-04 23:35:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Set the rightmost unset bit/set-the-rightmost-unset-bit.py
+++ Difficulty: Basic/Set the rightmost unset bit/set-the-rightmost-unset-bit.py
@@ -1,9 +1,12 @@
 #User function Template for python3
 class Solution:
-	def setBit(self, n):
-		# code here
-		
-		return n | (n + 1)
+    def setBit(self, n):
+        # code here
+        
+        return n | (n + 1)
+
+
+
 
 
 #{ 
