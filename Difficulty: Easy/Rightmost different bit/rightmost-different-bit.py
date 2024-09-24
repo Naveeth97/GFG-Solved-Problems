5bdc1df0e5859cace3901b72c030a1d54d038212
Naveeth Khan J <111632946+Naveeth97@users.noreply.github.com>
2024-09-24 09:29:59 -0700

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Rightmost different bit/rightmost-different-bit.py
@@ -0,0 +1,63 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+
+
+    
+# } Driver Code Ends
+#User function Template for python3
+
+class Solution:
+    
+    #Function to find the first position with different bits.
+    def posOfRightMostDiffBit(self,m,n):
+        #Your code here
+        ans = 0
+        
+        if m == n:
+            return -1
+            
+        while m > 0 or n > 0:
+            
+            ans += 1
+            if (m & 1) != (n & 1):
+                break
+                
+            m >>= 1
+            n >>= 1
+            
+        return ans
+
+#{ 
+ # Driver Code Starts.
+    
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        
+        mn=[int(x) for x in input().strip().split()]
+        m=mn[0]
+        n=mn[1]
+        ob=Solution()
+        print(math.floor(ob.posOfRightMostDiffBit(m,n)))
+        
+        
+        
+        
+        T-=1
+    
+    
+
+
+
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
