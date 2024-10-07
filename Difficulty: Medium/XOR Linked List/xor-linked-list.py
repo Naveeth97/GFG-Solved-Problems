3f8810d1624ff8e646eda7dbaee05b953125249b
Naveeth Khan J <111632946+Naveeth97@users.noreply.github.com>
2024-10-07 23:38:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/XOR Linked List/xor-linked-list.py
@@ -0,0 +1,49 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+def insert(head, data):
+    ptr=Node(data)
+    if head == None:
+       head=ptr
+    else:
+        ptr.npx=head
+        head=ptr
+    return ptr
+        
+    
+def getList(head):
+    ans=[]
+    while head != None:
+        ans.append(head.data)
+        head=head.npx
+    return ans
+    
+
+#{ 
+ # Driver Code Starts.
+#Back-end complete function Template for Python 3
+class Node:
+    def __init__(self, x):
+        self.data = x
+        self.npx = None
+
+def XOR(a, b):
+    return id(a) ^ id(b)
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        head = None
+        data_list = list(map(int, input().split()))
+        for data in data_list:
+            head = insert(head, data)
+        
+        vec = getList(head)
+        print(*vec)
+        print(*reversed(vec))
+
+# } Driver Code Ends
