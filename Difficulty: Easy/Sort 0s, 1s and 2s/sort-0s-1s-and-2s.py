773fdc3a71a7d64a76130bc75ada0a10a09e0f9b
Naveeth Khan J <111632946+Naveeth97@users.noreply.github.com>
2024-12-05 22:23:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.py
+++ Difficulty: Easy/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.py
@@ -1,7 +1,13 @@
+#{ 
+ # Driver Code Starts
+
+# } Driver Code Ends
 class Solution:
     # Function to sort an array of 0s, 1s, and 2s
     def sort012(self, arr):
         # code here
+        
+        #dutch national flag algorithm
         low, high = 0, len(arr) - 1
         
         mid = 0
@@ -10,7 +16,7 @@ class Solution:
             
             temp = arr[mid]
             
-            if (arr[mid] == 0):
+            if arr[mid] == 0:
                 arr[mid] = arr[low]
                 arr[low] = temp
                 mid += 1
@@ -30,8 +36,10 @@ class Solution:
         
 
 
+
+
 #{ 
- # Driver Code Starts
+ # Driver Code Starts.
 def main():
     t = int(input().strip())  # Read the number of test cases
     ob = Solution()
@@ -44,8 +52,8 @@ def main():
         ob.sort012(arr)  # Sort the array
 
         print(' '.join(map(str, arr)))  # Print the sorted array
-
-
+        print("~")
+        
 if __name__ == "__main__":
     main()
 
