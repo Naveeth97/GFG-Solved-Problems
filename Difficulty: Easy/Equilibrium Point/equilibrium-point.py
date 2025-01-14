4e5a399524a8b745e7c30af2b3e76b275ad1882b
Naveeth Khan J <111632946+Naveeth97@users.noreply.github.com>
2025-01-14 12:25:59 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Easy/Equilibrium Point/equilibrium-point.py
@@ -0,0 +1,51 @@
+# User function Template for python3
+
+class Solution:
+    #Function to find equilibrium point in the array.
+    def findEquilibrium(self, arr):
+        # code here
+        
+        total_sum = 0
+        
+        for i in range(len(arr)):
+            
+            total_sum += arr[i]
+            
+        left_sum = 0
+        
+        for i in range(len(arr)):
+            
+            left_sum += arr[i]
+            
+            if left_sum == total_sum:
+                return i
+                
+            total_sum -= arr[i]
+        return -1
+
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import math
+
+
+def main():
+    T = int(input())
+    while (T > 0):
+
+        arr = [int(x) for x in input().strip().split()]
+
+        ob = Solution()
+
+        print(ob.findEquilibrium(arr))
+        print("~")
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
