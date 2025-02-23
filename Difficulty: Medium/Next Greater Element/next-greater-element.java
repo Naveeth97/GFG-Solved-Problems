afe63e0da057ded2e70fd6c9577896ebc1314925
Naveeth Khan J <111632946+Naveeth97@users.noreply.github.com>
2025-02-23 22:57:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Next Greater Element/next-greater-element.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Geeks {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine()); // Number of test cases
+        for (int g = 0; g < t; g++) {
+            String[] str =
+                (br.readLine()).trim().split(" "); // Reading input as a string array
+            int arr[] = new int[str.length]; // Creating integer array from the input
+            for (int i = 0; i < str.length; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+            }
+
+            // Getting the result from the Solution class
+            ArrayList<Integer> result = new Solution().nextLargerElement(arr);
+
+            // Printing the result in the required format
+            if (result.isEmpty()) {
+                System.out.println("[]");
+            } else {
+                for (int i = 0; i < result.size(); i++) {
+                    if (i != 0) System.out.print(" ");
+                    System.out.print(result.get(i));
+                }
+                System.out.println();
+                System.out.println("~");
+            }
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to find the next greater element for each element of the array.
+    public ArrayList<Integer> nextLargerElement(int[] arr) {
+        // code here
+        
+        // referred in gfg portal solution, but understood clearly
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        ArrayList<Integer> nextGreater = new ArrayList<>();
+        
+        for (int i = 0; i < arr.length; i++) {
+            nextGreater.add(-1);
+        }
+        
+        
+        for (int i = arr.length - 1; i >= 0; i--) {
+            
+            while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+                stack.pop();
+            }
+            
+            if (!stack.isEmpty()) {
+                nextGreater.set(i, stack.peek());
+            }
+            
+            stack.push(arr[i]);
+        }
+        
+        return nextGreater;
+    }
+}
